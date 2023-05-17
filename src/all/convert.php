4482014ed149791e2c0934a72deaa09de09cc65f
Luiz Fernando Bernardes de Paula <nandovga@hotmail.com>
2023-05-17 19:12:06 -0300

    Update convert.php
    #??? - Criar função BuildTree

--- src/all/convert.php
+++ src/all/convert.php
@@ -22,3 +22,52 @@ if (!function_exists('EnumToArray')) {
         return $types;
     }
 }
+
+if (!function_exists('BuildTree')) {
+    /**
+     * Realiza a montagem da estrutura de dados para componente TreeList
+     * @param array $enum
+     * @param string|null $type
+     * @return array
+     */
+    function BuildTree(array $array, string $attr = "parent"): array
+    {
+        //Agrupa os dados pelo $attr
+        $a = array_reduce($array, function ($arr, $item) use ($attr) {
+            $value = $item[$attr];
+            if (!isset($arr[$value]))
+                $arr[$value] = array();
+            $arr[$value][] = $item;
+            return $arr;
+        }, array());
+        foreach ($a as $key => $value)
+            foreach ($value as $v)
+                $result[$key][] = $v;
+
+        $build = [];
+        foreach ($array as $key => $value)
+            if ($value[$attr] === null)
+                $build[] = $value;
+
+        /**
+         * Realiza a montage da estrutura de dados
+         * @param array $build
+         * @param array $result
+         * @return array
+         */
+        function genereteTree(array $build, array $result): array
+        {
+            $arr = [];
+            foreach ($build as $key => $value) {
+                $arr[$key] = $value;
+                if (isset($result[$value["id"]]))
+                    $arr[$key]["children"] = genereteTree($result[$value["id"]], $result);
+                else
+                    return $build;
+            }
+            return $arr;
+        }
+
+        return genereteTree($build, $result);
+    }
+}
